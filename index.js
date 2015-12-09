59ee370be48ebfc09295017ce9795dd60ae1fd67
Tracie Wamsley <traciewamsley@gmail.com>
2015-12-09 10:42:44 -0700

    script file added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express'),
     jade = require('jade'),
     path = require('path'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    route = require('./routes/routes.js');
+
 var app = express();
 
 app.set('view engine', 'jade'); 
