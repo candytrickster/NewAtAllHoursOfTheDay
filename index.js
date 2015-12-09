8085ea73767ff8c830260d2ada9dd624457ad407
Tracie Wamsley <traciewamsley@gmail.com>
2015-12-09 09:57:20 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+    jade = require('jade'),
+    path = require('path'),
+    bodyParser = require('body-parser');
+var app = express();
+
+app.set('view engine', 'jade'); 
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public'))); 
+
+urlencodedParser = bodyParser.urlencoded({ extended: false })
